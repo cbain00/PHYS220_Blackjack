7baf76bb025d162ab310af15d664aeec13e39c83
Christopher Bain <56360809+cbain00@users.noreply.github.com>
2021-04-23 21:44:28 -0400

    Update User Interactive Version of Code.py

--- User Interactive Version of Code.py
+++ User Interactive Version of Code.py
@@ -1,8 +1,9 @@
 import random
 
 # Constants
-USER_BALANCE = 1000 # Initial player balance
-NUM_OF_DECKS = 1 # Number of decks in use for play
+USER_BALANCE = 1000  # Initial player balance
+NUM_OF_DECKS = 1  # Number of decks in use for play
+
 
 def deal(deck):
     """
@@ -28,6 +29,7 @@ def deal(deck):
     hand.append(card2)
     return hand
 
+
 def hit(hand, deck):
     """
     A function that recreates the "hit" move in blackjack
@@ -50,6 +52,7 @@ def hit(hand, deck):
     hand.append(card)
     return hand
 
+
 def double_down(hand, deck, bet):
     """
     A function that recreates the "double down" move in blackjack
@@ -68,11 +71,14 @@ def double_down(hand, deck, bet):
         The player's updated hand
     bet : 
         The player's updated bet
+    deck:
+        Passed player's deck
 
     """
     bet += bet
     hand = hit(hand, deck)
     return hand, bet
+
 
 def hand_total(hand):
     """
@@ -90,7 +96,7 @@ def hand_total(hand):
 
     """
     hand_total = 0
-    
+
     # Adds each card value to hand_total
     for card in hand:
         # Adds 10 if card is a face card
@@ -107,6 +113,7 @@ def hand_total(hand):
             hand_total += card
     return hand_total
 
+
 def choose(hand):
     """
     Function that executes what move the player wants to make
@@ -122,34 +129,34 @@ def choose(hand):
         The player's choice
 
     """
-    choice = ""
     can_double_down = False
     inputString = "Do you want to Hit[H], Stand[S]"
-    
+
     # Adds double down option if player's hand allows for such a move
-    if (9 <= hand_total(hand) <= 11 and len(hand) == 2):
+    if 9 <= hand_total(hand) <= 11 and len(hand) == 2:
         inputString += ", Double Down[D]"
         can_double_down = True
     inputString += ": "
-    
+
     # Asks player what move they want to make 
-    
+
     # Includes double down move if applicable
     if can_double_down:
         choice = input(inputString).lower()
         while choice != "h" and choice != "s" and choice != "d":
             print("Please enter a valid choice")
             choice = input(inputString).lower()
-    
+
     # Just includes the hit and stand moves
     else:
         choice = input(inputString).lower()
         while choice != "h" and choice != "s":
             print("Please enter a valid choice")
             choice = input(inputString).lower()
-    
+
     return choice
-    
+
+
 def set_bet(balance):
     """
     Sets bet by asking player how much they want to bet
@@ -166,16 +173,23 @@ def set_bet(balance):
 
     """
     print("You have a total balance of $" + str(balance))
-    bet = int(input("How much do you want to bet? "))
-    
-    # Reasks player if their bet value is invalid
+    bet = input("How much do you want to bet? ")
+
+    while bet.isalpha():
+        print("Please enter a number amount to bet")
+        bet = input("How much do you want to bet? ")
+
+    bet = int(bet)
+
+    # Re-asks player if their bet value is invalid
     while bet < 0 or bet > balance:
         print("Please enter a valid bet")
         print("You have a total balance of $" + str(balance))
         bet = int(input("How much do you want to bet? "))
-        
+
     return bet
-        
+
+
 def game_win(player_hand_sum, dealer_hand_sum):
     """
     Determines whether the player or dealer won the game (round)
@@ -199,10 +213,10 @@ def game_win(player_hand_sum, dealer_hand_sum):
         return True
     elif player_hand_sum > dealer_hand_sum:
         return True
-    else: 
+    else:
         return False
 
-    
+
 def game(balance):
     """
     Function that actually runs a game/round of blackjack
@@ -220,58 +234,58 @@ def game(balance):
     """
     bet = set_bet(balance)
     player_choice = ""
-    
-    deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,
-            10,10,10,10,"J","J","J","J","Q","Q","Q","Q","K","K","K","K","A",
-            "A","A","A"] * NUM_OF_DECKS
-    
+
+    deck = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9,
+            10, 10, 10, 10, "J", "J", "J", "J", "Q", "Q", "Q", "Q", "K", "K", "K", "K", "A",
+            "A", "A", "A"] * NUM_OF_DECKS
+
     # Dealing initial hands
     player_hand = deal(deck)
-    dealer_hand = deal(deck)  
-    
+    dealer_hand = deal(deck)
+
     # Allows the player to make moves until they or the dealer wins (or if the 
     # player decides to stand or double down)
-    while (hand_total(player_hand) < 21 and hand_total(dealer_hand) < 21) and player_choice != "s" and player_choice != "d":
+    while (hand_total(player_hand) < 21 and hand_total(
+            dealer_hand) < 21) and player_choice != "s" and player_choice != "d":
         print("The dealer is showing a " + str(dealer_hand[0]))
-        print("You have a " + str(player_hand) + " for a total of " + 
-              str(hand_total(player_hand)))
+        print("You have a " + str(player_hand) + " for a total of " + str(hand_total(player_hand)))
         player_choice = choose(player_hand)
-        
+
         # The case if the player decides to hit
         if player_choice == "h":
             player_hand = hit(player_hand, deck)
             if hand_total(dealer_hand) < 17 and hand_total(player_hand) <= 21:
                 dealer_hand = hit(dealer_hand, deck)
-        
+
         # The case if the player decides to stand
         elif player_choice == "s":
             while hand_total(dealer_hand) < 17 and hand_total(player_hand) <= 21:
                 dealer_hand = hit(dealer_hand, deck)
-        
+
         # The case if the player decides to double down
         elif player_choice == "d":
             player_hand, bet = double_down(player_hand, deck, bet)
             while hand_total(dealer_hand) < 17 and hand_total(player_hand) <= 21:
                 dealer_hand = hit(dealer_hand, deck)
-    
+
     # The case if the player wins the game/round (their bet is added to their balance)
-    if (game_win(hand_total(player_hand), hand_total(dealer_hand))):
-        print("The dealer has a " + str(dealer_hand) + " for a total of " + 
-              str(hand_total(dealer_hand)))
-        print("You have a " + str(player_hand) + " for a total of " + 
-              str(hand_total(player_hand)))
+    if game_win(hand_total(player_hand), hand_total(dealer_hand)):
+        print("The dealer has a " + str(dealer_hand) + " for a total of " + str(hand_total(dealer_hand)))
+        print("You have a " + str(player_hand) + " for a total of " + str(hand_total(player_hand)))
+        if hand_total(player_hand) == 21:
+            print("You got a natural Blackjack!")
         print("You win $" + str(bet) + "!")
         balance += bet
-    
+
     # the case if the dealer wins the game/round (the player's bet is deducted from their balance)
     else:
-        print("The dealer has a " + str(dealer_hand) + " for a total of " + 
-              str(hand_total(dealer_hand)))
-        print("You have a " + str(player_hand) + " for a total of " + 
-              str(hand_total(player_hand)))
+        print("The dealer has a " + str(dealer_hand) + " for a total of " + str(hand_total(dealer_hand)))
+        print("You have a " + str(player_hand) + " for a total of " + str(hand_total(player_hand)))
+        if hand_total(dealer_hand) == 21:
+            print("Dealer got a natural Blackjack...")
         print("You lost $" + str(bet) + "!")
         balance -= bet
-        
+
     return balance
 
 
@@ -287,21 +301,32 @@ def main():
     """
     balance = USER_BALANCE
     choice = input("Welcome to Blackjack!\nWould you like to play? [Y] or [N]: ").lower()
-    
+    valid_choices = ['y', 'n']
+
+    # Input checking
+    while choice.lower() not in valid_choices or choice.isnumeric() is True:
+        print("Please enter 'Y' or 'N'")
+        choice = input("Welcome to Blackjack!\nWould you like to play? [Y] or [N]: ").lower()
+
     # Player plays new games/rounds if they decide to play and if they can enough 
     # money in their balance
-    while (choice == "y" and balance > 0):
+    while choice == "y" and balance > 0:
         balance = game(balance)
         print("\nYour current balance is $" + str(balance))
-        
-        if (balance <= 0):
+
+        if balance <= 0:
             print("You ran out of money!")
 
         elif balance > 0:
             choice = input("Would you like to play again? [Y] or [N]: ").lower()
-    
-    print("You ended with a balance of $" + str(balance))
 
-            
+        while choice.lower() not in valid_choices or choice.isnumeric() is True:
+            print("Please enter 'Y' or 'N'")
+            choice = input("Welcome to Blackjack!\nWould you like to play? [Y] or [N]: ").lower()
+
+    print("You ended with a balance of $" + str(balance))
+    print("\nThank you for playing Blackjack!")
+
+
 if __name__ == "__main__":
     main()
